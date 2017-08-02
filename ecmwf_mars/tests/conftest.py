5c1dd0a747ccd4359d022a1c39d125d6d7de6a49
Stefan Lüdtke <sluedtke@gfz-potsdam.de>
2017-08-02 23:29:23 +0200

    Add example and test for multiple years

--- ecmwf_mars/tests/conftest.py
+++ ecmwf_mars/tests/conftest.py
@@ -33,7 +33,6 @@ def example_1():
         "target": "output"
     }
     return(example_1)
-
 # -----------------------------------------------------------
 
 
@@ -57,5 +56,27 @@ def example_2():
         "target": "output"
     }
     return(example_2)
+# -----------------------------------------------------------
 
+
+@pytest.fixture(scope="session")
+def example_3():
+    """
+    Fixture that is the python equivalent to the example_3.txt.
+    """
+    example_3 = {
+        "class": "ei",
+        "dataset": "interim",
+        "date": "1979-01-01/to/1980-12-31",
+        "expver": "1",
+        "grid": "0.75/0.75",
+        "levtype": "sfc",
+        "param": "63.162/168.128",
+        "step": "0",
+        "stream": "oper",
+        "time": "00:00:00",
+        "type": "an",
+        "target": "output"
+    }
+    return(example_3)
 # -----------------------------------------------------------
--- ecmwf_mars/tests/test_mars.py
+++ ecmwf_mars/tests/test_mars.py
@@ -48,6 +48,7 @@ def test_parse_dates_mars_example_1(example_1, mars_example_1):
     assert len(mars_example_1.date_list) == 31
     assert mars_example_1.date_list[0] == '1979-01-01'
     assert mars_example_1.date_list[-1] == '1979-01-31'
+# -----------------------------------------------------------
 
 
 # Check whether the creating the object succeeds
@@ -85,3 +86,41 @@ def test_parse_dates_mars_example_2(example_2, mars_example_2):
     assert len(mars_example_2.date_list) == 59
     assert mars_example_2.date_list[0] == '1979-01-01'
     assert mars_example_2.date_list[-1] == '1979-02-28'
+# -----------------------------------------------------------
+
+
+# Check whether the creating the object succeeds
+def test_read_example_3():
+    temp = mars.mars_request("./ecmwf_mars/tests/example_data/example_3.txt")
+    assert isinstance(temp, mars.mars_request), 'Wrong data type'
+
+
+@pytest.fixture(scope="session")
+def mars_example_3():
+    temp = mars.mars_request("./ecmwf_mars/tests/example_data/example_3.txt")
+    return(temp)
+
+
+# Compare whether the parsed mars request is identical to the ECMWF output
+# (copied from the website)
+def test_read_mars_example_3(example_3, mars_example_3):
+    assert mars_example_3.request == example_3
+
+
+# Compare the number of parsed parameters
+def test_parse_para_mars_example_3(example_3, mars_example_3):
+    assert len(mars_example_3.para_list) == 2
+    assert mars_example_3.para_list == ['63.162', '168.128']
+
+
+# Compare the number of parsed years
+def test_parse_year_mars_example_3(example_3, mars_example_3):
+    assert len(mars_example_3.year_list) == 2
+    assert mars_example_3.year_list == [1979, 1980]
+
+
+# Compare the number of parsed dates
+def test_parse_dates_mars_example_3(example_3, mars_example_3):
+    assert len(mars_example_3.date_list) == 731
+    assert mars_example_3.date_list[0] == '1979-01-01'
+    assert mars_example_3.date_list[-1] == '1980-12-31'
